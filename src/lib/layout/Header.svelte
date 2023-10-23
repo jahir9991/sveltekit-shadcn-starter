912d0c9c7baec9ae652737e6dd6950da1f6eba3c
jahir alam <work.jahiralam@gmail.com>
2023-10-23 22:08:50 +0600

    ok

--- src/lib/layout/Header.svelte
+++ src/lib/layout/Header.svelte
@@ -6,7 +6,7 @@
 
 <header>
 	<div class="corner">
-		<a href="https://kit.svelte.dev">
+		<a href="/home">
 			<img src={logo} alt="SvelteKit" />
 		</a>
 	</div>
@@ -16,14 +16,20 @@
 			<path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
 		</svg>
 		<ul>
-			<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
-				<a href="/">Home</a>
+			<li aria-current={$page.url.pathname === '/home' ? 'page' : undefined}>
+				<a href="/home">Home</a>
 			</li>
-			<li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
+			<!-- <li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
 				<a href="/about">About</a>
 			</li>
 			<li aria-current={$page.url.pathname.startsWith('/sverdle') ? 'page' : undefined}>
 				<a href="/sverdle">Sverdle</a>
+			</li> -->
+			<li aria-current={$page.url.pathname.startsWith('/auth') ? 'page' : undefined}>
+				<a href="/auth">auth</a>
+			</li>
+			<li aria-current={$page.url.pathname.startsWith('/dash') ? 'page' : undefined}>
+				<a href="/dash">dashbord</a>
 			</li>
 		</ul>
 		<svg viewBox="0 0 2 3" aria-hidden="true">
--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,3 +1,7 @@
-// since there's no dynamic data here, we can prerender
-// it so that it gets served as a static asset in production
-export const prerender = true;
+import { redirect } from '@sveltejs/kit';
+import type { PageLoad } from './$types';
+
+export const load: PageLoad = async () => {
+
+	throw redirect(302, '/home');
+};
